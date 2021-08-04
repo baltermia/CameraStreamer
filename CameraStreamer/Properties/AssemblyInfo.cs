345f9c20b017e227ff46d60f035923598cf0cacc
speyck <54800864+speyck@users.noreply.github.com>
2021-08-04 15:13:33 +0200

    add nuspec file and edited assemblyinfo for release

--- CameraStreamer/Properties/AssemblyInfo.cs
+++ CameraStreamer/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CameraStreamer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("CameraStreamer is a simple facade of the Aforge library and allows you to stream your Computer Camera on desktop apps. It is optimized for WPF projects but can also work with WinForms or other projects.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CameraStreamer")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
